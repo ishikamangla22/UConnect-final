58c91f745530e7a8732ce0e0f28d709be880c146
ishikamangla22 <ishika1725.be21@chitkara.edu.in>
2024-04-24 11:40:12 +0530

    login authmanager
    login authmanager

--- UConnect/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ UConnect/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -7,6 +7,51 @@
 
 import UIKit
 
+private let usernameField: UITextField = {
+    let field = UITextField()
+    field.placeholder = "Username..."
+    field.returnKeyType = .next
+    field.leftViewMode = .always
+    field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+    field.autocapitalizationType = .none
+    field.autocorrectionType = .no
+    field.layer.masksToBounds = true
+    field.layer.cornerRadius = Constants.cornerRadius
+    field.backgroundColor = .secondarySystemBackground
+    field.layer.borderWidth = 1.0
+    field.layer.borderColor = UIColor.secondaryLabel.cgColor
+    return field
+}()
+
+private let registerButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Sign Up", for: .normal)
+    button.layer.masksToBounds = true
+    button.layer.cornerRadius = Constants.cornerRadius
+    button.backgroundColor = .systemBlue
+    button.setTitleColor(.white, for: .normal)
+    return button
+}()
+
+
+private let passwordField: UITextField = {
+    let field = UITextField()
+    field.isSecureTextEntry = true
+    field.placeholder = "Password..."
+    field.returnKeyType = .continue
+    field.leftViewMode = .always
+    field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+    field.autocapitalizationType = .none
+    field.autocorrectionType = .no
+    field.layer.masksToBounds = true
+    field.layer.cornerRadius = Constants.cornerRadius
+    field.backgroundColor = .secondarySystemBackground
+    field.layer.borderWidth = 1.0
+    field.layer.borderColor = UIColor.secondaryLabel.cgColor
+    return field
+}()
+
+
 class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
